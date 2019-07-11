4acd4afc508d69911d51adf951b33f4d7e40b6dc
Octavio Marchi Parisi <octavio.marchi@gmail.com>
2019-07-10 20:58:12 -0700

    String

--- src/com/variablesDatatypesOperators/VariablesDatatypesAndOperators.java
+++ src/com/variablesDatatypesOperators/VariablesDatatypesAndOperators.java
@@ -33,5 +33,8 @@ public class VariablesDatatypesAndOperators {
 
         boolean myBolean = false;
         boolean isMale = true;
+
+        String numberString = "258";
+        System.out.println("My new String is: " + (numberString + myIntValue));
     }
 }
